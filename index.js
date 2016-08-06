ac9db3de4f0e8c73fe24114152c447c7799d609b
Chris Fordham <chris@fordham-nagy.id.au>
2016-08-06 14:54:12 +1000

    Add uptime plugin.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = {
   plugins: [
     require('./lib/plugins/express_static_page.js'),
     require('./lib/plugins/ping_pong.js'),
+    require('./lib/plugins/uptime.js'),
   ],
   debug: process.env.BOTKIT_DEBUG,
   port: process.env.BOTKIT_PORT
